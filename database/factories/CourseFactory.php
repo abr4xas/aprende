c2d5f3ef9be1686e9c6d9dc473d918b0c27ba52d
angel cruz <angel.c@geopagos.com>
2021-06-12 14:51:36 -0300

    Fixed what needed fixing and squished some bugs. :bug:

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Course;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CourseFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Course::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+			'name' => $this->faker->catchPhrase,
+			'image' => 'https://images.unsplash.com/photo-1603349206295-dde20617cb6a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+			'price' => $this->faker->randomFloat(2, 0, null),
+			'slug',
+        ];
+    }
+}
